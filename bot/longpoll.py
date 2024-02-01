c78a51e53a84c9635291698304be0729b692d4cd
Vladislav Isakov <samprp.03@mail.ru>
2024-02-01 12:29:43 +0300

    Добавлено перечисление, для VKUserLongpoll, отвечающий за параметр longpoll_mode.

--- bot/longpoll.py
+++ bot/longpoll.py
@@ -1,8 +1,27 @@
+from bot.VKApi import VKApi
+from enum import IntEnum
 
+class VkLongpollMode(IntEnum):
+    """
+    Дополнительные параметры, для получения расширенных данных в событиях.
+    Документация: (https://dev.vk.com/ru/api/user-long-poll/getting-started)
+    """
+    # получать вложения
+    ATTACHMENTS = 2
+    # возвращать расширенный набор событий
+    EXTENDED_SET_OF_EVENTS = 16
+    # возвращать pts (это требуется для работы метода messages.getLongPollHistory без ограничения в 256 последних событий)
+    GET_PTS = 32
+    # в событии с кодом 8 (друг стал онлайн) возвращать дополнительные данные в поле $extra 
+    # (смотрите https://dev.vk.com/ru/api/user-long-poll/getting-started#Структура%20событий)
+    ADDITIONAL_DATA_EXTRA = 64
+    # возвращать поле random_id 
+    # (random_id может быть передан при отправке сообщения методом https://dev.vk.com/ru/method/messages.send).
+    RETURN_THE_RANDOM_ID_FIELD = 128
 
 class VKUserLongpoll:
 
-    def __init__(self) -> None:
+    def __init__(self, base_vk_api: VKApi, longpoll_wait: int = 25, longpoll_mode, longpoll_version) -> None:
         pass
 
 class VkBotLongPoll:
